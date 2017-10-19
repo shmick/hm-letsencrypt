5fca6e65f341f463b18d6a4c63e5c188e0789038
Steve M <shmick@users.noreply.github.com>
2017-10-18 20:57:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-set -x
-# sleep for 10 seconds to wait for the network to fully start
-sleep 10
 
 DIR=/mnt/mmcblk0p4
 SCRIPT=letsencrypt.sh
 
-if [ ! -f $DIR/$SCRIPT  ]
+if [ ! -f $DIR/$SCRIPT ]
 then 
 curl -so $DIR/$SCRIPT https://raw.githubusercontent.com/shmick/hm-letsencrypt/master/$SCRIPT
 chmod +x $DIR/$SCRIPT
