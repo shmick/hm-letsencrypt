501443f5de2efcfc3e8a276e712e5189db5ee70b
Steve M <shmick@users.noreply.github.com>
2017-10-18 20:24:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sleep 10
 DIR=/mnt/mmcblk0p4
 SCRIPT=letsencrypt.sh
 
-if [ -z $DIR/$SCRIPT  ]
+if [ ! -f $DIR/$SCRIPT  ]
 then 
 curl -o $DIR/SCRIPT https://raw.githubusercontent.com/shmick/hm-letsencrypt/master/$SCRIPT
 chmod +x $DIR/$SCRIPT
